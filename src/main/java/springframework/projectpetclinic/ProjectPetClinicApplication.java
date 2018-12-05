eb5abe174098b37f039cedd15e375077b0755479
alexander8buga <alexander8buga@gmail.com>
2018-12-05 11:24:44 -0800

    Initial Project Add

---
+++ src/main/java/springframework/projectpetclinic/ProjectPetClinicApplication.java
@@ -0,0 +1,12 @@
+package springframework.projectpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPetClinicApplication.class, args);
+    }
+}
