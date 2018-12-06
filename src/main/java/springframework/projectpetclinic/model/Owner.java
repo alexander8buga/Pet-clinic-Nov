d5004ae48903907103bbd26e15a54cca6799e2fb
alexander8buga <alexander8buga@gmail.com>
2018-12-06 15:53:19 -0800

    adding POJOs. Closes #2

---
+++ src/main/java/springframework/projectpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package springframework.projectpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/springframework/projectpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package springframework.projectpetclinic.model;
+
+public class Vet extends Person {
+
+}
