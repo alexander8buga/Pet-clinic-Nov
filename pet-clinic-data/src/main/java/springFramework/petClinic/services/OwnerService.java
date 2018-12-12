146ad9d29e2c3972e3b8e03295f80aa0b46d27c5
alexander8buga <alexander8buga@gmail.com>
2018-12-12 15:49:39 -0800

    Refactor Service Interface. closes #11

--- pet-clinic-data/src/main/java/springFramework/petClinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/springFramework/petClinic/services/OwnerService.java
@@ -2,10 +2,8 @@ package springFramework.petClinic.services;
 
 import springFramework.petClinic.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner, Long>{
 
-public interface OwnerService {
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
-;}
+    Owner findByLastName(String lastName);
+
+}
