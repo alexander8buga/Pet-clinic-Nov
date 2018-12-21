d8ca0cbae04f2f62b1f8f26756b8ac00729121f6
alexander8buga <alexander8buga@gmail.com>
2018-12-21 11:44:01 -0800

    added visit JPA Entity. closes #38

--- pet-clinic-data/src/main/java/springFramework/petClinic/model/Visit.java
+++ pet-clinic-data/src/main/java/springFramework/petClinic/model/Visit.java
@@ -1,11 +1,23 @@
 package springFramework.petClinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+/**
+ * Created by ab on 12/21/2018
+ */
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
+    @Column(name="local_data")
     private LocalDate localDate;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
     private Pet pet;
+
+    @Column(name="description")
     private String description;
 
 
