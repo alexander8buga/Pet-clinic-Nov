65a262344b9bf165893be97ec2950ae03eb9f6ba
alexander8buga <alexander8buga@gmail.com>
2018-12-21 11:31:57 -0800

    Convert Pets to JPA Entities. closes #37

--- pet-clinic-data/src/main/java/springFramework/petClinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/springFramework/petClinic/model/Specialty.java
@@ -1,6 +1,17 @@
 package springFramework.petClinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by ab on 12/21/2018
+ */
+@Entity
+@Table(name="specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/springFramework/petClinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springFramework/petClinic/model/Vet.java
@@ -1,10 +1,21 @@
 package springFramework.petClinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Create by ab on 12/21/2018
+ */
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
 
+    // JPA loads all at ones when using eager
+    @ManyToMany(fetch = FetchType.EAGER)
+    // join table is a special table so the entities will have their own relationship
+    //and DB will create a table that will define ids for relation btw entities
+    @JoinTable(name="vet_specialties", joinColumns = @JoinColumn(name="vet_id"), inverseJoinColumns = @JoinColumn(name="specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
