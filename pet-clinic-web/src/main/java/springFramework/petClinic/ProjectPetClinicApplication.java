1ed698cd6ee4c5f90f265f4275660373fc2b7811
alexander8buga <alexander8buga@gmail.com>
2019-01-03 09:46:30 -0800

    update to JUnit 5. closes #4

--- pet-clinic-web/src/main/java/springFramework/petClinic/ProjectPetClinicApplication.java
+++ pet-clinic-web/src/main/java/springFramework/petClinic/ProjectPetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectPetClinicApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ProjectPetClinicApplication.class, args);
     }
---
+++ pet-clinic-web/src/test/java/springframework/petClinic/ProjectPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package springframework.petClinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ProjectPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
